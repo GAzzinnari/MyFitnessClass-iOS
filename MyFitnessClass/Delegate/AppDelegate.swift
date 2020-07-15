36454f45ab03023adba2ee5a582365b761492b9c
Gabriel Azzinnari <gazzinnari.github@gmail.com>
2020-07-15 20:18:14 -0300

    Initial setup + Pod init

---
+++ MyFitnessClass/Delegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MyFitnessClass
+//
+//  Created by Gabriel on 7/15/20.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        return true
+    }
+
+}
+
